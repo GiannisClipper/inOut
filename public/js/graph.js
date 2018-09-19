3e0b3a9986c384cf2f3aa70979593abffd403139
GiannisClipper <42655167+GiannisClipper@users.noreply.github.com>
2018-09-19 08:48:46 +0300

    Add files via upload

--- public/js/graph.js
+++ public/js/graph.js
@@ -20,6 +20,18 @@ class Graph {
   set height(passval) {}
   get height() {return parseInt(window.getComputedStyle(this.svg).getPropertyValue('height'))}; //this.svg.height.animVal.value;}
 
+  get marginT() {return !this.margin.top?0:parseFloat(this.height)*this.margin.top/100;}
+  set marginT(passval) {this.margin.top=passval;}
+
+  get marginL() {return !this.margin.left?0:parseFloat(this.width)*this.margin.left/100;}
+  set marginL(passval) {this.margin.left=passval;}
+
+  get marginB() {return !this.margin.bottom?0:parseFloat(this.height)*this.margin.bottom/100;}
+  set marginB(passval) {this.margin.bottom=passval;}
+
+  get marginR() {return !this.margin.right?0:parseFloat(this.width)*this.margin.right/100;}
+  set marginR(passval) {this.margin.right=passval;}
+/*
   get marginT() {return (this.margin.top!==null)?this.margin.top:parseFloat(this.height)/10;}
   set marginT(passval) {this.margin.top=passval;}
 
@@ -31,7 +43,7 @@ class Graph {
 
   get marginR() {return (this.margin.right!==null)?this.margin.right:parseFloat(this.width)/20;}
   set marginR(passval) {this.margin.right=passval;}
-
+*/
   graphWidth() {return parseFloat(this.width)-this.marginL-this.marginR;}
   graphHeight() {return parseFloat(this.height)-this.marginT-this.marginB;}
 
@@ -107,6 +119,10 @@ class GraphScale extends Graph {
   constructor(passval) {
     super(passval);
     this.items.labels=[];
+    this.marginT=10;
+    this.marginL=10;
+    this.marginB=10;
+    this.marginR=1;
   }
 
   calcItems() {
@@ -144,7 +160,7 @@ class GraphScale extends Graph {
       retval+=`<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" style="stroke:rgb(128, 128, 128); stroke-width:1" />`;
       if (i>0) {
         x1-=this.graphWidth()/this.items.steps/2;
-        y1=this.marginT+this.graphHeight()+(this.marginB/2);
+        y1=this.marginT+this.graphHeight()+16; //(this.marginB/2);
         retval+=`<text x="${x1}" y="${y1}" text-anchor="middle" transform="rotate(345, ${x1}, ${y1})">${this.items.labels[i-1]}</text>`;
       }
     }
@@ -170,6 +186,9 @@ class GraphScale extends Graph {
 class GraphBars extends Graph {
   constructor(passval) {
     super(passval);
+    this.marginT=10;
+    this.marginL=10;
+    this.marginB=10;
     this.items.steps=8;
     this.range.tillSum=false;
   }
@@ -212,9 +231,9 @@ class GraphBars extends Graph {
     let x=0;
     let y=0;
     for (let i=0; i<this.items.steps; ++i) {
-      x=this.marginL+(i*(gap+itemWidth+gap))+gap+(itemWidth/(1+this.items.steps/10));
-      y=this.marginT+this.graphHeight()+(this.marginB/3);
-      retval+=`<text x="${x}" y="${y}" text-anchor="end" transform="rotate(${360-this.items.steps*2}, ${x}, ${y})">${this.data[i].name.substr(0,12)}</text>`;
+      x=this.marginL+(i*(gap+itemWidth+gap))+gap+itemWidth/2; //(itemWidth/(1+this.items.steps/10));
+      y=this.marginT+this.graphHeight()+16; //(this.marginB/3);
+      retval+=`<text x="${x}" y="${y}" text-anchor="middle" transform="rotate(${360-this.items.steps*2}, ${x}, ${y})">${this.data[i].name.substr(0,12)}</text>`;
     }
     return retval;
   }
@@ -236,6 +255,70 @@ class GraphBars extends Graph {
       x+=itemWidth/2;
       y-=(y<this.marginT+16)?-16:5;
       retval+=`<text x="${x}" y="${y}" text-anchor="middle">${this.data[i].percent}</text>`;
+    }
+    return retval;
+  }
+}
+
+class GraphPie extends Graph {
+  constructor(passval) {
+    super(passval);
+    this.marginT=10;
+    this.items.steps=8;
+  }
+
+  drawRange() {
+  }
+
+  calcItems() {
+    this.items.steps=Math.min(this.items.steps, this.data.length);
+    if (this.items.steps<this.data.length) {
+      this.data[this.items.steps-1].name='ΛΟΙΠΑ';
+      for (let i=this.items.steps; i<this.data.length; i++)
+        this.data[this.items.steps-1].amount+=this.data[i].amount;
+      this.data[this.items.steps-1].percent=100;
+      for (let i=0; i<this.items.steps-1; i++)
+        this.data[this.items.steps-1].percent-=parseInt(this.data[i].percent);
+      this.data[this.items.steps-1].percent=this.data[this.items.steps-1].percent.toFixed(0)+'%';
+    }
+  }
+
+polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  let angleInRadians=(angleInDegrees-90)*Math.PI/180.0;
+  return {
+    x:centerX+(radius*Math.cos(angleInRadians)),
+    y:centerY+(radius*Math.sin(angleInRadians))
+  };
+}
+
+calcArc(x, y, radius, fromAngle, tillAngle){
+  let from=this.polarToCartesian(x, y, radius, fromAngle);
+  let till=this.polarToCartesian(x, y, radius, tillAngle);
+  let largeArcFlag=tillAngle-fromAngle<=180?'0':'1';
+  let d=[
+    "M", from.x, from.y, 
+    "A", radius, radius, 0, largeArcFlag, 1, till.x, till.y,
+    "L", x, y
+    ].join(" ");
+    return d;       
+}
+
+  drawGraph() {
+    let colors=['Coral', 'CornflowerBlue', 'YellowGreen', 'Salmon', 'SkyBlue', 'Yellow', 'MediumSpringGreen ', 'LightGrey'];
+    let retval='';
+    let x=this.marginL+this.graphWidth()/2;
+    let y=this.marginT+this.graphHeight()/2;
+    let r=Math.min(this.graphWidth(), this.graphHeight())*0.45;
+    let offset=0;
+    let itemAngle=0;
+    for (let i=0; i<this.items.steps; ++i) {
+      itemAngle=(i<this.items.steps-1)?parseFloat(this.data[i].percent)/100*360:360-offset;
+      retval+=`<path fill="${colors[i]}" d="${this.calcArc(x, y, r, offset, offset+itemAngle)}" />`;
+      retval+=`<line x1="${this.polarToCartesian(x, y, r+r/15, offset+itemAngle/2).x}" y1="${this.polarToCartesian(x, y, r+r/15, offset+itemAngle/2).y}" x2="${this.polarToCartesian(x, y, r-r/15, offset+itemAngle/2).x}" y2="${this.polarToCartesian(x, y, r-r/15, offset+itemAngle/2).y}" style="stroke:rgb(0,0,0);stroke-width:1" />`
+      retval+=`<text x="${this.polarToCartesian(x, y, r+r/10, offset+itemAngle/2).x}" 
+                     y="${this.polarToCartesian(x, y, r+r/10, offset+itemAngle/2).y}"
+                     text-anchor="${offset+itemAngle/2<=180?'start':'end'}">${this.data[i].percent+' '+this.data[i].name.substr(0,12)}</text>`;
+      offset+=itemAngle;
     }
     return retval;
   }
